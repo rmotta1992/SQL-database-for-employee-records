b4a94081d6801bad95f15b0374be983c5493f9b3
rmotta1992 <68138899+rmotta1992@users.noreply.github.com>
2020-10-14 03:14:30 -0400

    adding first schema

---
+++ EmployeeSQL/table_schema.sql
@@ -0,0 +1,40 @@
+CREATE TABLE departments(
+	dept_no VARCHAR(30),
+	dept_name VARCHAR(30)
+);
+SELECT * FROM departments;
+
+CREATE TABLE employee_dept(
+	emp_no VARCHAR(30),
+	dept_no VARCHAR(30)
+);
+SELECT * FROM employee_dept
+
+CREATE TABLE dept_manager(
+	dept_no VARCHAR(30),
+	emp_no VARCHAR(30)
+);
+SELECT * FROM dept_manager
+
+CREATE TABLE employees(
+	emp_no INTEGER,
+	emp_title_id VARCHAR(30),
+	birth_date VARCHAR(30),
+	first_name VARCHAR(30),
+	last_name VARCHAR(30),
+	sex VARCHAR (1),
+	hire_date VARCHAR(30)
+);
+SELECT * FROM employees
+
+CREATE TABLE titles(
+	title_id VARCHAR(30),
+	title VARCHAR(30)
+);
+SELECT * FROM titles
+
+CREATE TABLE salaries( 
+	emp_no VARCHAR(30),
+	salary INTEGER
+);
+SELECT * FROM salaries
