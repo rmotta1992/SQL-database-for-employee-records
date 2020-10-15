f6a40f1a383dcd8e85fc7b66dd5143c2de2d9b04
rmotta1992 <68138899+rmotta1992@users.noreply.github.com>
2020-10-14 22:31:44 -0400

    adding employees in sales dept

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -26,3 +26,13 @@ ON employee_dept.dept_no = departments.dept_no;
 SELECT first_name, last_name, sex
 FROM EMPLOYEES
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+SELECT employee_dept.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+LEFT JOIN employee_dept
+ON employees.emp_no = employee_dept.emp_no 
+LEFT JOIN departments
+ON employee_dept.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales' 
+
+
