f28538d32827c370602c836513ecc50b27dcd0b1
rmotta1992 <68138899+rmotta1992@users.noreply.github.com>
2020-10-14 22:33:52 -0400

    cleaned up table schema for final submission

--- EmployeeSQL/table_schema.sql
+++ EmployeeSQL/table_schema.sql
@@ -2,19 +2,19 @@ CREATE TABLE departments(
 	dept_no VARCHAR(30),
 	dept_name VARCHAR(30)
 );
-SELECT * FROM departments;
+
 
 CREATE TABLE employee_dept(
 	emp_no INTEGER,
 	dept_no VARCHAR(30)
 );
-SELECT * FROM employee_dept
+
 
 CREATE TABLE dept_manager(
 	dept_no VARCHAR(30),
 	emp_no INTEGER
 );
-SELECT * FROM dept_manager
+
 
 CREATE TABLE employees(
 	emp_no INTEGER,
@@ -25,17 +25,16 @@ CREATE TABLE employees(
 	sex VARCHAR (1),
 	hire_date VARCHAR(30)
 );
-SELECT * FROM employees
+
 
 CREATE TABLE titles(
 	title_id VARCHAR(30),
 	title VARCHAR(30)
 );
-SELECT * FROM titles
 
 
 CREATE TABLE salaries( 
 	emp_no INTEGER,
 	salary INTEGER
 );
-SELECT * FROM salaries
+
