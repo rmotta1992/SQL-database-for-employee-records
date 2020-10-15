50714deaedebe71ca37289751097861e5ae0b0ea
rmotta1992 <68138899+rmotta1992@users.noreply.github.com>
2020-10-14 22:08:59 -0400

    adding second query

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -3,3 +3,7 @@ employees.sex, salaries.emp_no, salaries.salary
 FROM employees
 LEFT JOIN salaries
 ON employees.emp_no = salaries.emp_no;
+
+SElECT first_name, last_name, hire_date
+FROM employees
+WHERE DATE(hire_date) BETWEEN '1/1/1986' AND '12/31/1986';
