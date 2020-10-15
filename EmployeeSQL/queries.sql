ce136dc1e2dddbc8e83562bfc94513839dca9075
rmotta1992 <68138899+rmotta1992@users.noreply.github.com>
2020-10-14 22:32:08 -0400

    adding employees in sales dept and dev dept

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -35,4 +35,11 @@ LEFT JOIN departments
 ON employee_dept.dept_no = departments.dept_no
 WHERE departments.dept_name = 'Sales' 
 
+SELECT employee_dept.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+LEFT JOIN employee_dept
+ON employees.emp_no = employee_dept.emp_no 
+LEFT JOIN departments
+ON employee_dept.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Development';
 
