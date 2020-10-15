851ce6b4ac7173e2edf7e2ed747644a45ad7be04
rmotta1992 <68138899+rmotta1992@users.noreply.github.com>
2020-10-14 22:28:23 -0400

    adding managers query

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -7,3 +7,11 @@ ON employees.emp_no = salaries.emp_no;
 SElECT first_name, last_name, hire_date
 FROM employees
 WHERE DATE(hire_date) BETWEEN '1/1/1986' AND '12/31/1986';
+
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no,
+employees.last_name, employees.first_name
+FROM departments
+LEFT JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no
+LEFT JOIN employees
+ON dept_manager.emp_no = employees.emp_no;
